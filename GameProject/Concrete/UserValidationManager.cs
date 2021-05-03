c916dda2405f71c81fe8b713eaa3189f8959a41c
betulgecer <gecer.b13@gmail.com>
2021-05-03 07:59:29 +0300

    GameProject
    KampIntro/GameProject

---
+++ GameProject/Concrete/UserValidationManager.cs
@@ -0,0 +1,18 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Concrete
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(User user)
+        {
+            return true;
+        }
+
+    }
+
+}
